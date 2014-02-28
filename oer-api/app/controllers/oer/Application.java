d13073b2008f3b1ea38129427a8ff03a24a15a09
Fabian Steeg <fsteeg@gmail.com>
2014-02-28 17:11:25 +0100

    Set up basic PUT write API with authorization (#12)

--- oer-api/app/controllers/oer/Application.java
+++ oer-api/app/controllers/oer/Application.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
@@ -17,20 +18,43 @@ import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
+import org.mindrot.jbcrypt.BCrypt;
 
+import play.Logger;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.oer_index;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.base.Joiner;
+import com.google.common.io.BaseEncoding;
 
 public class Application extends Controller {
+
+	private static final String INDEX = "oer-index";
+	private static final String DATA_TYPE = "oer-type";
+	private static final String USER_TYPE = "users";
 
 	final static Client client = new TransportClient(ImmutableSettings
 			.settingsBuilder().put("cluster.name", "quaoar").build())
 			.addTransportAddress(new InetSocketTransportAddress(
 					"193.30.112.170", 9300));
+
+	/**
+	 * Create a new user account. Pass two arguments: username and password.
+	 */
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.err.println("Pass two arguments: username and password");
+			System.exit(-1);
+		}
+		String user = args[0];
+		String pass = BCrypt.hashpw(args[1], BCrypt.gensalt());
+		System.out.print(responseInfo(client
+				.prepareIndex(INDEX, USER_TYPE, user)
+				.setSource("user", user, "pass", pass).execute().actionGet()));
+	}
 
 	public static Result query(String q, String t) {
 		if (q.trim().isEmpty() && t.trim().isEmpty())
@@ -41,6 +65,43 @@ public class Application extends Controller {
 					"/oer?q=Africa&t=http://schema.org/CollegeOrUniversity")));
 					// @formatter:on@
 		return processQuery(q, t);
+	}
+
+	public static Result write(String id) {
+		if (!authorized())
+			return unauthorized("Not authorized to write data!\n");
+		JsonNode json = request().body().asJson();
+		if (json == null || !json.isObject())
+			return badRequest("Expecting single JSON object in request body!\n");
+		Logger.info("Storing under ID '{}' data from user '{}': {}", id,
+				userAndPass()[0], json);
+		try {
+			return ok(responseInfo(client.prepareIndex(INDEX, DATA_TYPE, id)
+					.setSource(json.toString()).execute().actionGet()));
+		} catch (Exception x) {
+			return internalServerError(x.getMessage());
+		}
+	}
+
+	private static boolean authorized() {
+		String[] userAndPass = userAndPass();
+		SearchResponse search = search(QueryBuilders.idsQuery(USER_TYPE).ids(
+				userAndPass[0]));
+		return search.getHits().getTotalHits() == 1
+				&& BCrypt.checkpw(userAndPass[1], (String) search.getHits()
+						.getAt(0).getSource().get("pass"));
+	}
+
+	private static String[] userAndPass() {
+		try {
+			String header = request().getHeader(AUTHORIZATION)
+					.replace("Basic", "").trim();
+			return new String(BaseEncoding.base64().decode(header), "UTF-8")
+					.split(":");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new String[] { "unauthorized", "" };
 	}
 
 	private static Result processQuery(String q, String t) {
@@ -57,11 +118,17 @@ public class Application extends Controller {
 	}
 
 	private static SearchResponse search(final QueryBuilder queryBuilder) {
-		SearchRequestBuilder requestBuilder = client.prepareSearch("oer-index")
+		SearchRequestBuilder requestBuilder = client.prepareSearch(INDEX)
 				.setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
 				.setQuery(queryBuilder);
 		SearchResponse response = requestBuilder.setFrom(0).setSize(50)
 				.setExplain(false).execute().actionGet();
 		return response;
+	}
+
+	private static String responseInfo(IndexResponse r) {
+		return String.format(
+				"Indexed into index %s, type %s, id %s, version %s\n",
+				r.getIndex(), r.getType(), r.getId(), r.getVersion());
 	}
 }
--- oer-api/build.sbt
+++ oer-api/build.sbt
@@ -4,7 +4,8 @@ version := "0.1.0-SNAPSHOT"
 
 libraryDependencies ++= Seq(
   cache,
-  "org.elasticsearch" % "elasticsearch" % "0.90.7" withSources()
-)     
+  "org.elasticsearch" % "elasticsearch" % "0.90.7" withSources(),
+  "org.mindrot" % "jbcrypt" % "0.3m" withSources()
+)
 
 play.Project.playJavaSettings
