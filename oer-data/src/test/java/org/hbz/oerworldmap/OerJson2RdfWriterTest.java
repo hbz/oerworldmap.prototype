807849c6d11c42579cecb524c77e779c45e099b3
Fabian Steeg <fsteeg@gmail.com>
2014-03-27 19:36:06 +0100

    Move data transformation code from tests to a Transform class (#5)
    Move the actual transformation code into a Transform class, call
    that from the tests. Add a main method to do the actual full
    transformation (run Transform.java as Java application).
    
    Run NtToEs.java in oer-api to index the result in Elasticsearch.

--- oer-data/src/test/java/org/hbz/oerworldmap/OerJson2RdfWriterTest.java
+++ oer-data/src/test/java/org/hbz/oerworldmap/OerJson2RdfWriterTest.java
@@ -8,16 +8,8 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 import org.apache.commons.io.FileUtils;
-import org.culturegraph.mf.morph.Metamorph;
-import org.culturegraph.mf.stream.pipe.StreamTee;
-import org.culturegraph.mf.stream.source.DirReader;
-import org.culturegraph.mf.stream.source.FileOpener;
 import org.junit.Test;
 import org.lobid.lodmill.AbstractIngestTests;
-import org.lobid.lodmill.PipeEncodeTriples;
-import org.lobid.lodmill.RdfModelFileWriter;
-import org.lobid.lodmill.Stats;
-import org.lobid.lodmill.Triples2RdfModel;
 
 /**
  * @author Pascal Christoph (dr0i)
@@ -28,38 +20,26 @@ public class OerJson2RdfWriterTest {
 	private static final String GEO_LIST = "small/geoList";
 	private static final String ORGANIZATION_ID = "small/organizationId";
 	private static final String CONSORTIUM_MEMBERS = "small/consortiumMembers";
-	// uncomment for transforming the whole data
-	// private static final String GEO_LIST = "geoList";
-	// private static final String ORGANIZATION_ID = "organizationId";
-	// private static final String CONSORTIUM_MEMBERS = "consortiumMembers";
+
 	private final String OCWC_PATH = "ocwc/";
 	private final String TARGET_PATH = "tmp/";
 	private final String TEST_FILENAME = "ocwcTestResult.nt";
-
-	private static RdfModelFileWriter createWriter(final String PATH) {
-		final RdfModelFileWriter writer = new RdfModelFileWriter();
-		writer.setProperty("http://purl.org/dc/elements/1.1/identifier");
-		writer.setEndIndex(1);
-		writer.setStartIndex(0);
-		writer.setFileSuffix("nt");
-		writer.setSerialization("N-TRIPLE");
-		writer.setTarget(PATH);
-		return writer;
-	}
 
 	@Test
 	public void testFlow() {
 		try {
 			FileUtils.deleteQuietly(new File(TARGET_PATH));
-			transformDataInDirectory(OCWC_PATH + CONSORTIUM_MEMBERS);
-			transformDataInDirectory(OCWC_PATH + ORGANIZATION_ID);
-			transformDataInDirectory(OCWC_PATH + GEO_LIST);
+			Transform.dataInDirectory(TARGET_PATH, OCWC_PATH
+					+ CONSORTIUM_MEMBERS);
+			Transform.dataInDirectory(TARGET_PATH, OCWC_PATH + ORGANIZATION_ID);
+			Transform.dataInDirectory(TARGET_PATH, OCWC_PATH + GEO_LIST);
 			File testFile;
-			testFile = AbstractIngestTests.concatenateGeneratedFilesIntoOneFile(TARGET_PATH,
-					TARGET_PATH + OCWC_PATH + TEST_FILENAME);
-			AbstractIngestTests.compareFilesDefaultingBNodes(testFile, new File(Thread
-					.currentThread().getContextClassLoader().getResource(OCWC_PATH + TEST_FILENAME)
-					.toURI()));
+			testFile = AbstractIngestTests
+					.concatenateGeneratedFilesIntoOneFile(TARGET_PATH,
+							TARGET_PATH + OCWC_PATH + TEST_FILENAME);
+			AbstractIngestTests.compareFilesDefaultingBNodes(testFile,
+					new File(Thread.currentThread().getContextClassLoader()
+							.getResource(OCWC_PATH + TEST_FILENAME).toURI()));
 			FileUtils.deleteDirectory(new File(TARGET_PATH));
 		} catch (final FileNotFoundException e) {
 			e.printStackTrace();
@@ -68,31 +48,5 @@ public class OerJson2RdfWriterTest {
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
-	}
-
-	private void transformDataInDirectory(final String pathToDirectory) throws URISyntaxException {
-		final DirReader dirReader = new DirReader();
-		final FileOpener opener = new FileOpener();
-		final JsonDecoder jsonDecoder = new JsonDecoder();
-		final Metamorph morph = new Metamorph(Thread.currentThread().getContextClassLoader()
-				.getResource("morph-ocwConsortiumMembers-to-rdf.xml").getFile());
-		final PipeEncodeTriples encoder = new PipeEncodeTriples();
-		encoder.setStoreUrnAsUri("true");
-		final Triples2RdfModel triple2model = new Triples2RdfModel();
-		triple2model.setInput("N-TRIPLE");
-		final RdfModelFileWriter writer = OerJson2RdfWriterTest.createWriter(TARGET_PATH
-				+ pathToDirectory);
-		final StreamTee streamTee = new StreamTee();
-		final Stats stats = new Stats();
-		streamTee.addReceiver(stats);
-		streamTee.addReceiver(encoder);
-		encoder.setReceiver(triple2model).setReceiver(writer);
-		opener.setReceiver(jsonDecoder);
-		jsonDecoder.setReceiver(morph).setReceiver(streamTee);
-		dirReader.setReceiver(opener);
-		dirReader.process((new File(Thread.currentThread().getContextClassLoader()
-				.getResource(pathToDirectory).toURI())).getAbsolutePath());
-		opener.closeStream();
-
 	}
 }
