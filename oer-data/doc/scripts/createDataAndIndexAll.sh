a30b513bad0325e9a33c2c22ed8b8df5214ad5b2
pascal Christoph <christoph@hbz-nrw.de>
2014-08-12 11:33:57 +0200

    Rename createDataAndIndexAll.sh to startCreateDataAndIndexAll.sh
    * add copy resources into jar
    * use variable

--- oer-data/doc/scripts/createDataAndIndexAll.sh
+++
@@ -1,8 +0,0 @@
-# to install and index: 
-# create the genoames data and index that
-# create (Transform) the ocwc and wsis data , partyl via API lookups
-	# create OCWC description itself, copy that to "output"
-	# convert these ntriples (residing in the output/ ) to Json and index that
-
-cd ../../;  mvn assembly:assembly  -DdescriptorId=jar-with-dependencies -DskipTests; java -classpath classes:target/oer-data-0.1.0-jar-with-dependencies.jar  org.hbz.oerworldmap.Geonames; java -classpath classes:target/oer-data-0.1.0-jar-with-dependencies.jar org.hbz.oerworldmap.Transform; cd  doc/scripts/ ; bash createOcwcDescription.sh ; cd ../../; java -classpath classes:target/oer-data-0.1.0-jar-with-dependencies.jar  org.hbz.oerworldmap.NtToEs
-
